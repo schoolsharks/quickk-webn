48f561fc17a16a1a7029efc1c97ef350cedc6ed7
Your Name <you@example.com>
2025-09-13 11:20:39 +0530

    Text Change at event section.

--- client/src/features/events/components/Events.tsx
+++ client/src/features/events/components/Events.tsx
@@ -1,21 +1,40 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 // import EventsToday from "./EventsToday";
 // import UpcomingEvents from "./UpcomingEvents";
-import InterestingEvent from "./InterestingEvent";
+// import InterestingEvent from "./InterestingEvent";
 // import Recordings from "./Recordings";
 
 const Events = () => {
   return (
-    <Box
-      p={"8px 26px 60px 26px"}
-      display={"flex"}
-      flexDirection={"column"}
-      gap={8}
-    >
-      {/* <EventsToday /> */}
-      <InterestingEvent />
-      {/* <UpcomingEvents /> */}
-      {/* <Recordings /> */}
+    <Box>
+      <Box
+        position={"absolute"}
+        zIndex={2}
+        sx={{
+          width: "80%",
+          left: "50%",
+          top: "80%",
+          transform: "translate(-50%,-50%)",
+          textAlign: "center",
+          padding: "0 0px",
+        }}
+      >
+        <Typography fontSize={"22px"} fontWeight={"700"}>
+          ✨ Something exciting is brewing!
+        </Typography>
+      </Box>
+      <Box
+        p={"8px 26px 60px 26px"}
+        display={"flex"}
+        flexDirection={"column"}
+        minHeight={"40vh"}
+        gap={8}
+      >
+        {/* <EventsToday /> */}
+        {/* <InterestingEvent /> */}
+        {/* <UpcomingEvents /> */}
+        {/* <Recordings /> */}
+      </Box>
     </Box>
   );
 };

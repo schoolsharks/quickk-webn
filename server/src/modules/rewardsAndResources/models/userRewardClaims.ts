2541adb905508faefc230bdeddd4f3d4048d5d6e
Manish <[manishbulchandani4@gmail.com]>
2025-09-25 16:04:32 +0530

    rewards section

--- server/src/modules/rewardsAndResources/models/userRewardClaims.ts
+++ server/src/modules/rewardsAndResources/models/userRewardClaims.ts
@@ -2,6 +2,7 @@ import { Schema, model } from "mongoose";
 import { IUserRewardsClaims } from "../types/interface";
 import { RewardTypes } from "../types/enums";
 
+<<<<<<< HEAD
 const UserRewardsClaimsSchema = new Schema<IUserRewardsClaims>(
   {
     user: {
@@ -47,6 +48,23 @@ UserRewardsClaimsSchema.index({ user: 1, rewardType: 1, resourceId: 1 }, {
   partialFilterExpression: { rewardType: "RESOURCES" }
 });
 UserRewardsClaimsSchema.index({ createdAt: -1 });
+=======
+
+
+// Write a controller and service in theuserrewardclaims feature, it will and also route, take example of other features structuring, it should  be apply for reward, sending the type, and we will take the user from auth middleware, and then validate if it is in the RewardType or not, and if yes then we will create a document in userreward claims, and also it have to check if the current user has 1000 stars, if he dont have, then we will reject, and otherwise deduct 1000 stars from user, for that we need to use userservice, also 
+
+// implement this thing
+const UserRewardsClaimsSchema = new Schema<IUserRewardsClaims>({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  rewardType: {
+    type: String,
+    enum: Object.values(RewardTypes),
+  },
+});
+>>>>>>> ce5b79d (rewards section)
 
 const UserRewardsClaims = model<IUserRewardsClaims>(
   "UserRewardsClaims",
